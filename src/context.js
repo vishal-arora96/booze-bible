902694c08e31d8cfbc143a011eba62f4f1e5095c
Vishal Arora <vishal.arora@practo.com>
2023-04-20 03:05:19 +0530

    Remove unused import

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useCallback } from "react";
 
 const apiURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
